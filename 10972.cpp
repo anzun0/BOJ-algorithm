dc0eb68c64049b59f124245aba518558131d48a7
ajyng <zxzimin@naver.com>
2021-01-10 23:57:10 +0900

    21/01/10

---
+++ 10972.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+
+int main() {
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    
+    if (next_permutation(v.begin(), v.end())) {
+        for (int i = 0; i < n; i++)
+            cout << v[i] << ' ';
+    } else {
+        cout << -1 << '\n';
+    }
+}
---
+++ 10974.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+
+int main(void) {
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) v[i] = i + 1;
+    
+    do {
+        for (int i = 0; i < n; i++)
+            cout << v[i] << ' ';
+        cout << '\n';
+    } while (next_permutation(v.begin(), v.end()));
+    
+    return 0;
+}
