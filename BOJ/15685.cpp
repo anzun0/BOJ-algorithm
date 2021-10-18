d1585520c5f5658e5d8e00173c5c7022fffac6b7
anzun0 <zxzimin@naver.com>
2021-10-18 23:56:53 +0900

    21/10/18

---
+++ BOJ/15685.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int arr[100][100];
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int y, x, d, g;
+        cin >> y >> x >> d >> g;
+
+
+    }
+
+
+}
